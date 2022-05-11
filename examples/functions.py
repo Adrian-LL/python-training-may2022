e5552cc14310d886dd3aab976ebaed91cf934662
Iulia Chiriac <iulia.chiriac@yahoo.com>
2022-05-11 16:15:32 +0300

    Add modules/packages examples

--- examples/functions.py
+++ examples/functions.py
@@ -5,17 +5,18 @@ def diff(first, second=0):  # a is required, b is optional
     return first - second
 
 
-# Call diff with positional arguments
-result = diff(10, 2)
-print(result)
+if __name__ == '__main__':
+    # Call diff with positional arguments
+    result = diff(10, 2)
+    print(result)
 
-# Call diff without specifying a value for the optional argument b
-result = diff(10)
-print(result)
+    # Call diff without specifying a value for the optional argument b
+    result = diff(10)
+    print(result)
 
-# Call diff with keyword arguments
-result = diff(second=2, first=10)
-print(result)
+    # Call diff with keyword arguments
+    result = diff(second=2, first=10)
+    print(result)
 
 
 def varargs(*args, **kwargs):
@@ -23,28 +24,31 @@ def varargs(*args, **kwargs):
     print(kwargs, type(kwargs))
 
 
-varargs()
-varargs('hello')
-varargs('hello', 'world')
-varargs('hello', 'world', '!')
+if __name__ == '__main__':
+    varargs()
+    varargs('hello')
+    varargs('hello', 'world')
+    varargs('hello', 'world', '!')
 
-varargs(name="Ana", age=20)
+    varargs(name="Ana", age=20)
 
-varargs(1, 2, 3, name="Ana", age=20)
+    varargs(1, 2, 3, name="Ana", age=20)
 
 
 def all_arguments_types(a, b, *args, c, x=10, y=20, **kwargs):
     print(a, b, c, args, x, y, kwargs)
 
 
-all_arguments_types(1, 2, c=3)  # c is a required keyword-only argument
+if __name__ == '__main__':
+    all_arguments_types(1, 2, c=3)  # c is a required keyword-only argument
 
 
 def print_clone(*values, sep=' ', end='\n', file=sys.stdout, flush=False):
-    print(type(values))
+    # print(type(values))
     print(*values, sep=sep, end=end, file=file, flush=flush)
 
 
-print_clone("hello", "world", sep="***")
-print_clone("hello", "world", end="***")
-print_clone('a', 'b', 'c')
+if __name__ == '__main__':
+    print_clone("hello", "world", sep="***")
+    print_clone("hello", "world", end="***")
+    print_clone('a', 'b', 'c')
---
+++ examples/import_examples.py
@@ -0,0 +1,27 @@
+# import functions
+#
+# print(functions.diff(10, 20))
+# functions.print_clone('hello', 'world')
+
+# import functions as ff
+#
+# print(ff.diff(10, 20))
+# ff.print_clone('hello', 'world')
+
+
+# from functions import diff
+#
+# print(diff(10, 20))
+
+
+# from functions import diff as difference
+#
+# print(difference(10, 20))
+
+# from pypackage import pymodule
+# pymodule.func()
+
+# from pypackage.pymodule import func
+# func()
+
+import pypackage
